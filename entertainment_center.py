3badc59cb4d8069f35f7b7544457a19675fab972
James Harding <james@jamesharding.me>
2017-08-13 15:31:34 +0100

    initial setup. Add entertainment_center.py script and Movie class (media.py)

---
+++ entertainment_center.py
@@ -0,0 +1,11 @@
+import media
+import fresh_tomatoes
+
+
+def init():
+    movies = [media.Movie('Toy Story'), media.Movie('A bug\'s life')]
+
+    fresh_tomatoes.open_movies_page(movies)
+
+
+init()
---
+++ media.py
@@ -0,0 +1,8 @@
+class Movie:
+
+    poster_image_url = '123'
+    trailer_youtube_id = '123'
+    trailer_youtube_url = '123'
+
+    def __init__(self, title):
+        self.title = title
