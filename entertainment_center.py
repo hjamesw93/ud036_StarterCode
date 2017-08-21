1a4ab675074728f1c72961a6757ab5cd3d5fd816
James Harding <james@jamesharding.me>
2017-08-21 23:13:52 +0100

    Add entertainment_center and add functionality for media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,10 +1,36 @@
+import json
 import media
 import fresh_tomatoes
 
 
-def init():
-    movies = [media.Movie('Toy Story'), media.Movie('A bug\'s life')]
+def main():
+    """
+    Loads movie data from json and creates Movie objects, appending to movies list.
+    Calls open_movies_page function from fresh tomatoes, passing movies list as parameter.
+    """
+
+    movies = []
+
+    # Load movie data from json file
+    data = open('assets/data/movies.json', 'r')
+    movie_list = json.loads(data.read())
+
+    # Create new Movie object for each movie listed in json file
+    for movie_data in movie_list:
+        movie = media.Movie()
+
+        movie\
+            .set_title(movie_data['title'])\
+            .set_poster_image_url(movie_data['poster_image_url'])\
+            .set_trailer_youtube_id(movie_data['trailer_youtube_id'])\
+            .set_trailer_youtube_url(movie_data['trailer_youtube_url'])
+
+        # Add movie object to movies list
+        movies.append(movie)
 
     fresh_tomatoes.open_movies_page(movies)
 
-init()
+    # Cleanup - close movie data file
+    data.close()
+
+main()
--- media.py
+++ media.py
@@ -1,8 +1,50 @@
 class Movie:
+    """
+    Defines the structure of a Movie instance to be consumed by fresh_tomatoes.py.
+    'Setter' functions return self to facilitate function chaining.
+    """
 
-    poster_image_url = '123'
-    trailer_youtube_id = '123'
-    trailer_youtube_url = '123'
+    def __init__(self):
+        """
+        Movie constructor. Takes no additional parameters to enforce use of setter functions
+        """
+        self.title = ""
+        self.poster_image_url = ""
+        self.trailer_youtube_id = ""
+        self.trailer_youtube_url = ""
 
-    def __init__(self, title):
-        self.title = title
+    def set_title(self, title):
+        """
+        Set the movie title
+        :param title:
+        :return self:
+        """
+        self.poster_image_url = title
+        return self
+
+    def set_poster_image_url(self, poster_image_url):
+        """
+        Set the poster image url
+        :param poster_image_url:
+        :return self:
+        """
+        self.poster_image_url = poster_image_url
+        return self
+
+    def set_trailer_youtube_id(self, trailer_youtube_id):
+        """
+        Set the youtube video id for the movie trailer
+        :param trailer_youtube_id:
+        :return:
+        """
+        self.trailer_youtube_id = trailer_youtube_id
+        return self
+
+    def set_trailer_youtube_url(self, trailer_youtube_url):
+        """
+        Set the youtube video url for the movie trailer
+        :param trailer_youtube_url:
+        :return:
+        """
+        self.trailer_youtube_url = trailer_youtube_url
+        return self
